1b132b73b5673855f713c851c29ff7ccb03cd73d
Thomas Davis <thomasalwyndavis@gmail.com>
2013-07-30 05:02:30 +0000

    city builder

---
+++ preview/build.js
@@ -0,0 +1,44 @@
+var superagent = require('superagent');
+var _ = require('underscore');
+var fs = require('fs');
+var http = require('http');
+var htmlMinifier = require('html-minifier');
+var uglifyJs = require('uglify-js');
+
+superagent.get('https://s3.amazonaws.com/cdnjs-artifacts//packages.json?' + new Date().getTime(), function(res, textStatus, xhr){
+  var packages = res.body.packages;
+  var indexTemplate = fs.readFileSync('index.template', 'utf8');
+  var indexPage = _.template(indexTemplate, {packages: packages});
+
+  var htmlCompressionOptions = {
+  	removeComments: true,
+  	collapseBooleanAttributes: true,
+  	collapseWhitespace: true,
+  };
+
+  indexPage = htmlMinifier.minify(indexPage, htmlCompressionOptions);
+  indexPage = indexPage.replace('&nbsp;', ' ');
+
+  fs.writeFileSync('index.html', indexPage, 'utf8');
+  fs.writeFileSync('packages.json', JSON.stringify(res.body, null, 4), 'utf8');
+  fs.writeFileSync('packages.min.json', JSON.stringify(res.body), 'utf8');
+
+  var result = uglifyJs.minify(['cdnjs.handlebars.js', 'index.js']);
+  fs.writeFileSync('min.js', result.code, 'utf8');
+});
+
+// I was rushing below r0fl
+var file = fs.createWriteStream("rss.xml");
+var request = http.get("http://s3.amazonaws.com/cdnjs-artifacts//rss", function(response) {
+  response.pipe(file);
+});
+
+
+
+var file2 = fs.createWriteStream("atom.xml");
+var request2 = http.get("http://s3.amazonaws.com/cdnjs-artifacts//rss", function(response) {
+  response.pipe(file2);
+});
+
+
+
