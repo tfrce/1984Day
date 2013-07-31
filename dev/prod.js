b1c0fbf249525b2d3c14da559ae66f56dc12578d
Thomas Davis <thomasalwyndavis@gmail.com>
2013-08-01 05:27:46 +1000

    minified html and cdn version

--- dev/prod.js
+++ dev/prod.js
@@ -1,9 +1,17 @@
+
 var fs = require('fs-extra');
 var cleanCSS = require('clean-css');
 var _ = require('lodash');
 var glob = require('glob');
 //var cdn = 'http://1984cdn.taskforce.is';
 var cdn = 'http://d3csjzoia27npz.cloudfront.net';
+var cdnversion = new Date().getTime();
+var htmlMinifier = require('html-minifier');
+var htmlCompressionOptions = {
+  	removeComments: true,
+  	collapseBooleanAttributes: true,
+  	collapseWhitespace: true,
+  };
 
 fs.copy('.', '..', function(err){
   if (err) {
@@ -15,7 +23,8 @@ fs.copy('.', '..', function(err){
     fs.writeFileSync('../css/styles.css', minimized, 'utf8');
 
     var indexPage = fs.readFileSync('../index.html', 'utf8');
-    indexPage = indexPage.replace('css/styles.css',cdn+'/css/styles.css');
+    indexPage = indexPage.replace('css/styles.css',cdn+'/css/styles.css?'+cdnversion);
+    indexPage = htmlMinifier.minify(indexPage, htmlCompressionOptions);
     fs.writeFileSync('../index.html', indexPage, 'utf8');
     glob("../**/index.html", {}, function (er, files) {
       var files = _.filter(files, function(file){
@@ -23,7 +32,8 @@ fs.copy('.', '..', function(err){
       });
       _.each(files, function(filename){
         var indexPage = fs.readFileSync(filename, 'utf8');
-        indexPage = indexPage.replace('../css/styles.css',cdn+'/css/styles.css');
+        indexPage = indexPage.replace('../css/styles.css',cdn+'/css/styles.css?'+cdnversion);
+        indexPage = htmlMinifier.minify(indexPage, htmlCompressionOptions);
         fs.writeFileSync(filename, indexPage, 'utf8');
 
       });
