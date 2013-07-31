87dc1a811f56d2075a1eb92a2d3bdfe223f459ec
Thomas Davis <thomasalwyndavis@gmail.com>
2013-08-01 04:44:29 +1000

    minimize css

--- dev/prod.js
+++ dev/prod.js
@@ -1,9 +1,15 @@
 var fs = require('fs-extra');
+var cleanCSS = require('clean-css');
+
+
 fs.copy('.', '..', function(err){
   if (err) {
     console.error(err);
   }
   else {
+    var styles = fs.readFileSync('../css/styles.css', 'utf8');
+    var minimized = cleanCSS.process(styles);
+    fs.writeFileSync('../css/styles.css', minimized, 'utf8');
     console.log("success!")
   }
 });
