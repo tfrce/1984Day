a6a0833cfc7bbce71f1198f6edcd4577c9bc17ac
Thomas Davis <thomasalwyndavis@gmail.com>
2013-08-01 05:15:25 +1000

    possibly worst build script ever but meh

--- dev/prod.js
+++ dev/prod.js
@@ -1,6 +1,14 @@
 var fs = require('fs-extra');
 var cleanCSS = require('clean-css');
-
+var _ = require('lodash');
+var glob = require('glob');
+glob("../**/index.html", {}, function (er, files) {
+  var files = _.filter(files, function(file){
+    return file.substr(0,6) !== '../dev' && file.substr(0, 13) !== '../index.html' && file.substr(0,7) !== '../node'
+  });
+console.log(files);
+})
+var cdn = 'http://1984cdn.taskforce.is';
 
 fs.copy('.', '..', function(err){
   if (err) {
@@ -10,6 +18,10 @@ fs.copy('.', '..', function(err){
     var styles = fs.readFileSync('../css/styles.css', 'utf8');
     var minimized = cleanCSS.process(styles, {relativeTo: '../css'});
     fs.writeFileSync('../css/styles.css', minimized, 'utf8');
+
+    var indexPage = fs.readFileSync('../index.html', 'utf8');
+    indexPage = indexPage.replace('css/styles.css',cdn+'/css/styles.css');
+    fs.writeFileSync('../index.html', indexPage, 'utf8');
     console.log("success!")
   }
 });
